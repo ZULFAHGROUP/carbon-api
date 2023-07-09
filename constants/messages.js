97b29f82c7eb932b2a33a1ff0ebc4f73485c77b3
Ayom04 <abdullahayomide04@gmail.com>
2023-07-09 10:25:59 +0100

    bugfix in constants

--- constants/messages.js
+++ constants/messages.js
@@ -1,20 +1,17 @@
-const { walletBalance } = require("../controllers/walletController");
-
 const messages = {
-    loginMessage: "User logged-in successfully",
-    registerMessage: "Guy, Your account has been created successfully, an otp has been sent to your email",
-    userExists: "User with the Phone/Email exists",
-    notFoundMessage: "What you seek is beyond this globe",
-    invalidOtp :  "Invalid otp",
-    walletBalanceMessage : "wallet balance fetched successfully",
-    errorBalancemessage: "Error fetching wallet balance",
-    errorSendMoneyMessage : "invalid amount or phone",
-    errorSendMoneyMessageLimit : "your transfer limit has been exceeded",
-    sendMoneyErrorRecipientDetails : "user not found",
-    userWalletDetailsError : "insufficient balance. Please top-up your wallet",
-    receipientSuccessmessage: "Transaction completed successfully"
- 
-    
-}
+  loginMessage: "User logged-in successfully",
+  registerMessage:
+    "Guy, Your account has been created successfully, an otp has been sent to your email",
+  userExists: "User with the Phone/Email exists",
+  notFoundMessage: "What you seek is beyond this globe",
+  invalidOtp: "Invalid otp",
+  walletBalanceMessage: "wallet balance fetched successfully",
+  errorBalancemessage: "Error fetching wallet balance",
+  errorSendMoneyMessage: "invalid amount or phone",
+  errorSendMoneyMessageLimit: "your transfer limit has been exceeded",
+  sendMoneyErrorRecipientDetails: "user not found",
+  userWalletDetailsError: "insufficient balance. Please top-up your wallet",
+  receipientSuccessmessage: "Transaction completed successfully",
+};
 
 module.exports = messages;
